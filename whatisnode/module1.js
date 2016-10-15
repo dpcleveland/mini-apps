9c2317107ef023cab4e9f51c895f622a08b2a0c8
Daniel Cleveland <dpcleveland@gmail.com>
2016-10-15 16:49:40 -0500

    what is node
    experimenting with node and npm

---
+++ whatisnode/module1.js
@@ -0,0 +1,13 @@
+var _ = require('underscore');
+var $ = require('jquery');
+var m2 = require('./module2');
+var http = require('http');
+
+var server = http.createServer(function(request, response){
+    console.log('got a request');
+    response.write('hi');
+    response.end();
+});
+
+server.listen(3000);
+m2();
