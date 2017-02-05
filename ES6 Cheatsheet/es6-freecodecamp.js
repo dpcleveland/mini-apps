aab53ae6aa714cc2f38016b96601890adb0961ce
Daniel Cleveland <dpcleveland@gmail.com>
2017-02-04 23:45:35 -0600

    Let vs var

--- ES6 Cheatsheet/es6-freecodecamp.js
+++ ES6 Cheatsheet/es6-freecodecamp.js
@@ -5,3 +5,20 @@ let favorite = redNosedReindeer + " is Santa's favorite reindeer.";
 
 ////////////////
 
+"use strict";
+var newCampers = [{camper: "Wil"}, {camper: "Sam"}, {camper: "Dav"}];
+// only change code below this line
+for (let i = 0; i < newCampers.length; i++) {
+// only change code above this line
+  newCampers[i].roleCall = function() {
+    return "Camper # " + (i + 1) + " has arrived.";
+  };
+}
+// test your code
+console.log(newCampers[0].roleCall());
+console.log(newCampers[1].roleCall());
+console.log(newCampers[2].roleCall());
+
+////////////////
+
+
